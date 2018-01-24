c90f0fa1249a3b88c9d23e41093b64db63b09b70
Patryk Roguszewski <patryk.roguszewski@gmail.com>
2018-01-24 18:54:28 +0100

    add more logging, and delete docker compose from solution

--- Docker.Webhook.Deploy/Controllers/DeployController.cs
+++ Docker.Webhook.Deploy/Controllers/DeployController.cs
@@ -3,6 +3,7 @@ using Docker.Webhook.Deploy.Models;
 using Docker.Webhook.Deploy.Services;
 using Docker.Webhook.Deploy.Settings;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -13,14 +14,17 @@ namespace Docker.Webhook.Deploy.Controllers
     {
         private readonly IDockerService _dockerService;
         private readonly CredentialsSettings _credentialsSettings;
-        public DeployController(IDockerService dockerService, CredentialsSettings credentialsSettings)
+        private readonly ILogger _logger;
+        public DeployController(IDockerService dockerService, CredentialsSettings credentialsSettings, ILogger<DeployController> logger)
         {
             _dockerService = dockerService;
             _credentialsSettings = credentialsSettings;
+            _logger = logger;
         }
         [HttpGet]
         public async Task<IActionResult> Deploy(DockerQuery dockerQuery)
         {
+            _logger.LogInformation($"Got http get to deploy controller, with args DockerRepository {dockerQuery.DockerRepository} DockerContainerName {dockerQuery.DockerContainerName}");
             await _dockerService.LoginAsync(_credentialsSettings.Login, _credentialsSettings.Password);
             var response = await _dockerService.UpdateImageAsync(dockerQuery.DockerRepository, dockerQuery.DockerContainerName);
             return Ok(response);
--- Docker.Webhook.Deploy/Services/DockerService.cs
+++ Docker.Webhook.Deploy/Services/DockerService.cs
@@ -1,23 +1,29 @@
 ﻿using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 
 namespace Docker.Webhook.Deploy.Services
 {
     public class DockerService : IDockerService, IService
     {
         private readonly IShellService _shellService;
-        public DockerService(IShellService shellService)
+        private readonly ILogger _logger;
+        public DockerService(IShellService shellService, ILogger<DockerService> logger)
         {
             _shellService = shellService;
+            _logger = logger;
         }
 
         public async Task LoginAsync(string login, string password)
         {
-            await _shellService.RunBashScriptAsync("login", $"login password");
+            var result = await _shellService.RunBashScriptAsync("login", $"login password");
+            _logger.LogInformation($"Got result from login async {result}");
         }
 
         public async Task<string> UpdateImageAsync(string dockerRepository, string dockerContainerName)
         {
-            return await _shellService.RunBashScriptAsync("update", $"{dockerRepository} {dockerContainerName}");
+            var result = await _shellService.RunBashScriptAsync("update", $"{dockerRepository} {dockerContainerName}");
+            _logger.LogInformation($"Got result from updateImage async {result}");
+            return result;
         }
     }
 }
