530327d64c30362437ad0af9ef2b810ff09de91b
Patryk Roguszewski <patryk.roguszewski@gmail.com>
2018-01-24 18:28:02 +0100

    change docker page query to base64 encoded parameters

--- Docker.Webhook.Deploy/Models/DockerQuery.cs
+++ Docker.Webhook.Deploy/Models/DockerQuery.cs
@@ -3,15 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Docker.Webhook.Deploy.Models
 {
     public class DockerQuery
     {
         [FromQuery]
-        public string DockerContainerName { get; set; }
+        public string DockerContainerName
+        {
+            get => _dockerContainerName;
+            set => _dockerContainerName = Base64UrlEncoder.Decode(value);
+        }
 
         [FromQuery]
-        public string DockerRepository { get; set; }
+        public string DockerRepository
+        {
+            get => _dockerRepository;
+            set => _dockerRepository = Base64UrlEncoder.Decode(value);
+
+        }
+
+        private string _dockerContainerName;
+
+        private string _dockerRepository;
     }
 }
